8b351c00dcaf9b847ed6d5d1881f616a089913e1
gpogoncheff <galenpogoncheff@me.com>
2019-04-17 11:05:15 -0600

    Initial training script for unet and unet implementation

---
+++ UNet.py
@@ -0,0 +1,92 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+
+
+class UNetDecoderModule(nn.Module):
+    def __init__(self, in_channels, in_channels_ct, out_channels):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.in_channels_ct = in_channels_ct
+        self.out_channels = out_channels
+
+        self.conv = nn.Conv2d(in_channels, in_channels_ct, kernel_size=3, padding=1)
+        self.transpose_conv = nn.ConvTranspose2d(in_channels_ct, out_channels, kernel_size=3,
+                                                 stride=2, padding=1, output_padding=1)
+        self.act = nn.ReLU(inplace=True)
+
+    def forward(self, X):
+        output = self.conv(X)
+        output = self.act(output)
+        output = self.transpose_conv(output)
+        output = self.act(output)
+        return output
+
+
+class UNet(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+
+        self.num_classes = num_classes
+
+        vgg11_encoder = models.vgg11(pretrained=False).features
+        self.encoder1 = vgg11_encoder[0]
+        self.encoder2 = vgg11_encoder[3]
+        self.encoder3 = vgg11_encoder[6]
+        self.encoder4 = vgg11_encoder[8]
+        self.encoder5 = vgg11_encoder[11]
+        self.encoder6 = vgg11_encoder[13]
+        self.encoder7 = vgg11_encoder[16]
+        self.encoder8 = vgg11_encoder[18]
+        self.encoder_act = vgg11_encoder[1]
+        self.max_pool = vgg11_encoder[2]
+
+        self.decoder6 = UNetDecoderModule(512, 512, 256)
+        self.decoder5 = UNetDecoderModule(256 + self.encoder8.out_channels, 512, 256)
+        self.decoder4 = UNetDecoderModule(256 + self.encoder6.out_channels, 512, 128)
+        self.decoder3 = UNetDecoderModule(128 + self.encoder4.out_channels, 256, 64)
+        self.decoder2 = UNetDecoderModule(64 + self.encoder2.out_channels, 128, 32)
+        self.decoder1 = nn.Sequential(
+            nn.Conv2d(32 + self.encoder1.out_channels, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, self.num_classes, kernel_size=1),
+        )
+
+    def forward(self, X):
+        '''
+        Encode
+        '''
+        # Skip connection
+        encoder1_out = self.encoder_act(self.encoder1(X))
+        encoder1_out_pooled = self.max_pool(encoder1_out)
+
+        # Skip connection
+        encoder2_out = self.encoder_act(self.encoder2(encoder1_out_pooled))
+        encoder2_out_pooled = self.max_pool(encoder2_out)
+
+        encoder3_out = self.encoder_act(self.encoder3(encoder2_out_pooled))
+        # Skip connection
+        encoder4_out = self.encoder_act(self.encoder4(encoder3_out))
+        encoder4_out_pooled = self.max_pool(encoder4_out)
+
+        encoder5_out = self.encoder_act(self.encoder5(encoder4_out_pooled))
+        # Skip connection
+        encoder6_out = self.encoder_act(self.encoder6(encoder5_out))
+        encoder6_out_pooled = self.max_pool(encoder6_out)
+
+        encoder7_out = self.encoder_act(self.encoder7(encoder6_out_pooled))
+        # Skip connection
+        encoder8_out = self.encoder_act(self.encoder8(encoder7_out))
+        encoder8_out_pooled = self.max_pool(encoder8_out)
+
+        '''
+        Decode
+        '''
+        decoder6_out = self.decoder6(encoder8_out_pooled)
+        decoder5_out = self.decoder5(torch.cat([decoder6_out, encoder8_out], 1))
+        decoder4_out = self.decoder4(torch.cat([decoder5_out, encoder6_out], 1))
+        decoder3_out = self.decoder3(torch.cat([decoder4_out, encoder4_out], 1))
+        decoder2_out = self.decoder2(torch.cat([decoder3_out, encoder2_out], 1))
+        decoder1_out = self.decoder1(torch.cat([decoder2_out, encoder1_out], 1))
+        return decoder1_out
---
+++ train.py
@@ -0,0 +1,124 @@
+import torch
+import torch.nn as nn
+from torchvision import transforms, datasets
+from UNet import UNet
+from PIL import Image
+from data_transforms import PILToLongTensor
+import numpy as np
+import os
+import sys
+import argparse
+import time
+
+
+def train(model, dataloader, criterion, optimizer, num_epochs, ckpt_path=None):
+    since = time.time()
+    min_loss = float('inf')
+
+    if ckpt_path is not None:
+        print('Resuming training from ckeckpoint...')
+        ckpt = torch.load(ckpt_path)
+        model.load_state_dict(ckpt['model_state_dict'])
+        optimizer.load_state_dict(ckpt['optimizer_state_dict'])
+        epoch = ckpt['epoch']
+        min_loss = ckpt['loss']
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 11)
+
+        running_loss = 0.0
+
+        # Iterate over data.
+        for inputs, labels in dataloader:
+
+            # zero the parameter gradients
+            optimizer.zero_grad()
+
+            with torch.set_grad_enabled(True):
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+
+                loss.backward()
+                optimizer.step()
+
+            # statistics
+            running_loss += loss.item() * inputs.size(0)
+
+        epoch_loss = running_loss / len(dataloader.dataset)
+
+        print('Training Loss: {}'.format(epoch_loss))
+
+        if epoch_loss < min_loss:
+            min_loss = epoch_loss
+
+            torch.save({
+                'epoch': epoch,
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),
+                'loss': epoch_loss
+            }, './checkpoints/unet.tar')
+
+        print()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+
+    return model
+
+
+def load_data(path):
+    input_transform = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor()
+    ])
+
+    output_transform = transforms.Compose([
+        transforms.Resize((256, 256), Image.NEAREST),
+        PILToLongTensor()
+    ])
+
+    dataset = datasets.Cityscapes(path, split='train', mode='fine',
+                                  target_type='semantic', transform=input_transform,
+                                  target_transform=output_transform)
+
+    return dataset
+
+
+
+def set_parameter_required_grad(model, requires_grad=True):
+    for param in model.parameters():
+        param.requires_grad = requires_grad
+    return
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Train UNet on CityScapes data')
+    parser.add_argument('path', type=str,
+                        help='Relative path to directory containing to CityScapes gtFine and leftImg8bit directories')
+    parser.add_argument('--ckpt', type=str, default=None, help='Relative path to saved checkpoint')
+    parser.add_argument('-b', '--batch_size', type=int, default=8, help='Image batch size (default 0.001)')
+    parser.add_argument('-lr', '--learning_rate', type=float, default=0.001, help='Learning rate (default 0.001)')
+    parser.add_argument('--epochs', type=int, default=100, help='Maximum number of training epochs')
+    args = parser.parse_args()
+
+    if (args.ckpt is not None) and (not os.path.exists(args.ckpt)):
+        sys.exit('Specified checkpoint cannot be found')
+
+    dataset = load_data(args.path)
+    sampler = torch.utils.data.SubsetRandomSampler(np.arange(10))
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, sampler=sampler)
+    #dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
+
+    model = UNet(num_classes=len(datasets.Cityscapes.classes))
+    set_parameter_required_grad(model, True)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
+    train(model, dataloader, criterion, optimizer, num_epochs=args.epochs, ckpt_path=args.ckpt)
+    return
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
