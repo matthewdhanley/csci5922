7b28cabde930e6521c826301ab951cda8016d233
gpogoncheff <galenpogoncheff@me.com>
2019-04-29 15:27:39 -0600

    Tests for data_loader

--- tests/test_utils_data_loader.py
+++ tests/test_utils_data_loader.py
@@ -1,7 +1,9 @@
 import unittest
 import sys
+import numpy as np
 import torch
-from torchvision import datasets
+from torchvision import transforms, datasets
+from PIL import Image
 
 sys.path.insert(0, '../')
 from utils import data_loader
@@ -10,12 +12,40 @@ class TestDataLoader(unittest.TestCase):
     def setUp(self):
         self.cityscapes_path = '../cityscapes'
         self.imagenet_path = '../tinyimagenet'
+        self.image = Image.fromarray(np.uint8(np.random.randint(0,256,(32,32))))
+
 
     def test_input_image_transform(self):
-        pass
+        # Test with no resizing
+        transform = data_loader.input_image_transform(0)
+        image_tensor = transform(self.image)
+        self.assertIsInstance(transform, transforms.Compose)
+        self.assertIsInstance(image_tensor, torch.Tensor)
+        self.assertEqual(image_tensor.size(), torch.Size([1,32,32]))
+
+        # Test with resizing to 16x16
+        transform = data_loader.input_image_transform(16)
+        image_tensor = transform(self.image)
+        self.assertIsInstance(transform, transforms.Compose)
+        self.assertIsInstance(image_tensor, torch.Tensor)
+        self.assertEqual(image_tensor.size(), torch.Size([1,16,16]))
+
 
     def test_output_image_transform(self):
-        pass
+        # Test with no resizing
+        transform = data_loader.output_image_transform(0)
+        image_tensor = transform(self.image)
+        self.assertIsInstance(transform, transforms.Compose)
+        self.assertIsInstance(image_tensor, torch.Tensor)
+        self.assertEqual(image_tensor.size(), torch.Size([32,32]))
+
+        # Test with resizing to 16x16
+        transform = data_loader.output_image_transform(16)
+        image_tensor = transform(self.image)
+        self.assertIsInstance(transform, transforms.Compose)
+        self.assertIsInstance(image_tensor, torch.Tensor)
+        self.assertEqual(image_tensor.size(), torch.Size([16,16]))
+
 
     def test_load_data(self):
         # Test loading of cityscapes dataset, no resize
