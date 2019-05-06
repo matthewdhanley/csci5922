21e51f7181bfa5396eaf18221ba6783ccb66463d
gpogoncheff <galenpogoncheff@me.com>
2019-05-05 18:00:42 -0600

    changed activation file name back to original naming scheme

--- retrieve_activations.py
+++ retrieve_activations.py
@@ -11,7 +11,7 @@ def retrieve_activations(net, dataloader, dataset_name):
     for inp, _ in dataloader:
         net(inp)
         activs.append(net.activs)
-    file = open("activations/{}_{}_activations".format(model_name, dataset_name), "wb")
-    print("Saving activations to activations/{}_{}_activations".format(model_name, dataset_name))
+    file = open("activations/{}_activations".format(model_name), "wb")
+    print("Saving activations to activations/{}_activations".format(model_name))
     pickle.dump(activs, file)
     file.close()
